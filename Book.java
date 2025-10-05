dda8015e1fc92de11720062ff180824ab95d2ecd
Moris L. Porteza <morisporteza60@gmail.com>
2025-10-05 21:20:42 +0800

    Update Book.java

---
+++ Book.java
@@ -0,0 +1,55 @@
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private int currentPage;
+
+    // Constructor
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.currentPage = 0; // default
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getYearPublished() {
+        return yearPublished;
+    }
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    // Setter for author (used in the assignment)
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Method to change the current page
+    public void changePage(int page) {
+        if (page < 0) {
+            System.out.println("Page number cannot be negative. No change made.");
+            return;
+        }
+        this.currentPage = page;
+    }
+
+    // Display details method (used in the assignment)
+    public void displayDetails() {
+        System.out.println("Title: " + title +
+                           ", Author: " + author +
+                           ", Year: " + yearPublished +
+                           ", Current Page: " + currentPage);
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + " (" + yearPublished + ")";
+    }
+}
